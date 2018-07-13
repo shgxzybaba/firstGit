f29535e4fbcbee0ee78edc9cc6d6c5fddfc2f8ae
akinduro-pc <akinduro-pc@segun-pc>
2018-07-13 03:03:17 +0100

    A database app for schools, haven't added view layer and front end yet

---
+++ src/main/java/com/shgxzyjune/personalproject/PersonalprojectApplication.java
@@ -0,0 +1,12 @@
+package com.shgxzyjune.personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalprojectApplication.class, args);
+	}
+}
