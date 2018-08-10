444b97906466f5e65587c9f4e48013d648f0c9eb
shgxzybaba <shgxzybaba@gmail.com>
2018-08-11 00:29:36 +0100

    A database app for schools, haven't added view layer and front end yet

--- src/main/java/com/shgxzyjune/personalproject/PersonalprojectApplication.java
+++ src/main/java/com/shgxzyjune/personalproject/PersonalprojectApplication.java
@@ -2,7 +2,7 @@ package com.shgxzyjune.personalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//entrypoint
 @SpringBootApplication
 public class PersonalprojectApplication {
 
